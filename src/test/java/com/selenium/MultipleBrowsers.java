eff5ea5be46bfb0ec4a5c9fd8ef0b199b6230f2c
Maksim Evstafev <emaxiss@gmail.com>
2020-11-23 23:43:52 -0500

    multiplebrosers testing example

--- src/test/java/com/selenium/MultipleBrowsers.java
+++ src/test/java/com/selenium/MultipleBrowsers.java
@@ -7,26 +7,39 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 class MultipleBrowsers
 {
 	private WebDriver driver;
 	private String title = "SeleniumHQ Browser Automation";
-	
+	private String browser = "edge";
+
 	@BeforeEach
 	void setup()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
-		driver = new ChromeDriver();
+		switch (browser)
+		{
+			case "chrome" -> {
+				System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+				driver = new ChromeDriver();}
+			case "firefox" -> {
+				System.setProperty("webdriver.chrome.driver", "C:/drivers/geckodriver.exe");
+				driver = new FirefoxDriver();}
+			case "edge" -> {
+				System.setProperty("webdriver.chrome.driver", "C:/drivers/msedgedriver.exe");
+				driver = new EdgeDriver();}
+		}
 	}
-	
+
 	@Test
 	void test()
 	{
 		driver.get("https://www.selenium.dev/");
 		assertEquals(title, driver.getTitle());
 	}
-	
+
 	@AfterEach
 	void tearDown()
 	{
