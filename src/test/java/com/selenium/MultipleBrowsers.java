b147dc6c0bb9e00b8ede91ee764c4dd024120f99
Maksim Evstafev <emaxiss@gmail.com>
2020-11-25 23:01:51 -0500

    add browser config

--- src/test/java/com/selenium/MultipleBrowsers.java
+++ src/test/java/com/selenium/MultipleBrowsers.java
@@ -1,8 +1,12 @@
 package com.selenium;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
@@ -14,22 +18,36 @@ class MultipleBrowsers
 {
 	private WebDriver driver;
 	private String title = "SeleniumHQ Browser Automation";
-	private String browser = "edge";
+	private static String browser;
+
+	@BeforeAll
+	static void oneTimeSetUp() throws IOException
+	{
+		Properties prop = new Properties();
+		prop.load(new FileInputStream("src/test/java/com/selenium/browser.config"));
+		browser = prop.getProperty("browser");
+	}
 
 	@BeforeEach
 	void setup()
 	{
 		switch (browser)
 		{
-			case "chrome" -> {
+			case "chrome" ->
+			{
 				System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
-				driver = new ChromeDriver();}
-			case "firefox" -> {
-				System.setProperty("webdriver.chrome.driver", "C:/drivers/geckodriver.exe");
-				driver = new FirefoxDriver();}
-			case "edge" -> {
-				System.setProperty("webdriver.chrome.driver", "C:/drivers/msedgedriver.exe");
-				driver = new EdgeDriver();}
+				driver = new ChromeDriver();
+			}
+			case "firefox" ->
+			{
+				System.setProperty("webdriver.gecko.driver", "C:/drivers/geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+			case "edge" ->
+			{
+				System.setProperty("webdriver.edge.driver", "C:/drivers/msedgedriver.exe");
+				driver = new EdgeDriver();
+			}
 		}
 	}
 
@@ -37,7 +55,7 @@ class MultipleBrowsers
 	void test()
 	{
 		driver.get("https://www.selenium.dev/");
-		assertEquals(title, driver.getTitle());
+		assertEquals(title, driver.getTitle()); // add AssertJ
 	}
 
 	@AfterEach
